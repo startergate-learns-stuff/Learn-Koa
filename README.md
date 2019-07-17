0a9650fe9618fcbc35c3ace4867a37a74b6ec0e8
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-17 23:39:03 +0900

    우와 간단하다아

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Learn Koa
 No one: <br>
 Me: Abandon Express!
-![](https://media.giphy.com/media/3o6ZtihIv0pJqdp3H2/giphy.gif)
+![Abandon Ship!](https://media.giphy.com/media/3o6ZtihIv0pJqdp3H2/giphy.gif)
 
 
 Koa.js를 공부하는 리포지토리입니다
--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
+router.get('/', (ctx, next) => {
+    ctx.body = '홈';
+});
+
+router.get('/about', (ctx, next) => {
+    ctx.body = '소개';
+});
+
+router.get('/about/:name', (ctx, next) => {
+    const { name } = ctx.params;
+    ctx.body = name + '의 소개'
+});
+
+router.get('/post', (ctx, next) => {
+    const { id } = ctx.request.query;
+    if (id) {
+        ctx.body = '포스트 #' + id;
+    } else {
+        ctx.body = '포스트 아이디가 없습니다.';
+    }
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
 app.listen(4000, () => {
     console.log(4000)
 });
