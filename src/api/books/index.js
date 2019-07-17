469dada4657a9885a203af91b3cb5c1c0a41fc88
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-17 23:52:43 +0900

    .

--- src/api/books/index.js
+++ src/api/books/index.js
@@ -3,10 +3,6 @@ const booksCtrl = require('./books.controller');
 
 const router = new Router();
 
-const handler = (ctx, next) => {
-    ctx.body = `${ctx.request.method} ${ctx.request.path}`
-};
-
 router.get('/', booksCtrl.list);
 router.post('/', booksCtrl.create);
 router.delete('/', booksCtrl.delete);
