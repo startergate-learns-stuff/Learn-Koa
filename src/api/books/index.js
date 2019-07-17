37109c1f2d14b0d14e84c32077b09fa67b77a091
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-17 23:52:12 +0900

    1-4

--- src/api/books/index.js
+++ src/api/books/index.js
@@ -1,9 +1,16 @@
 const Router = require('koa-router');
+const booksCtrl = require('./books.controller');
 
 const router = new Router();
 
-router.get('/', (ctx, next) => {
-    ctx.body = 'GET ' + ctx.request.path;
-});
+const handler = (ctx, next) => {
+    ctx.body = `${ctx.request.method} ${ctx.request.path}`
+};
+
+router.get('/', booksCtrl.list);
+router.post('/', booksCtrl.create);
+router.delete('/', booksCtrl.delete);
+router.put('/', booksCtrl.replace);
+router.patch('/', booksCtrl.update);
 
 module.exports = router;
