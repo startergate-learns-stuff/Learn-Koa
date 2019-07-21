8f38e683c8d7d070fc06d1c33b360620b08a44ee
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-07-21 21:21:18 +0900

    2-5 조금

--- src/api/books/books.controller.js
+++ src/api/books/books.controller.js
@@ -1,8 +1,31 @@
+const Book = require('models/book');
+
 exports.list = (ctx) => {
     ctx.body = 'listed';
 };
 
-exports.create = (ctx) => {
+exports.create = async (ctx) => {
+    const {
+        title,
+        authors,
+        publishedDate,
+        price,
+        tags
+    } = ctx.request.body;
+
+    const book = new Book({
+        title,
+        authors,
+        publishedDate,
+        price,
+        tags
+    });
+
+    try {
+        await book.save();
+    } catch (e) {
+        return ctx.throw(500, e);
+    }
     ctx.body = 'created';
 };
 
